bd272b212235664202d8fcb5884ecdd059705638
AlejandraM09 <a.montoya3@outlook.com>
2026-04-27 20:19:43 -0500

    Delete vetlearn directory

--- vetlearn/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-import { auth } from '@/lib/auth'
-import { redirect } from 'next/navigation'
-
-export default async function Home() {
-  const session = await auth()
-  if (session) redirect('/dashboard')
-  redirect('/login')
-}
